09a3e3ea13a4c7b86f44a104aa25582b5c509b64
Kyriosone <kyrios@inbox.ru>
2014-06-18 15:31:18 +0400

    trying actionbar

--- src/ru/Kyrios/TaskApp/Main.java
+++ src/ru/Kyrios/TaskApp/Main.java
@@ -2,14 +2,37 @@ package ru.Kyrios.TaskApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar.TabListener;
+import android.util.Log;
+import android.view.*;
+import android.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class Main extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+public class Main extends ActionBarActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
+
